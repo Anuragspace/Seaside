51671e834ebebd08c3a354eacbbd96e9fa710cc8
Anuragspace <anuraglife2020@gmail.com>
2025-06-22 17:19:48 +0530

    updated minor fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Hero from './components/Hero';
+import HomePage from './pages/HomePage';
 import RoomPage from './pages/RoomPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Hero />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/room/:roomId" element={<RoomPage />} />
     </Routes>
   );
