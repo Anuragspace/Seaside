ee12e52f571b9042e5b785d4a2c33e832de386a6
Anuragspace <anuraglife2020@gmail.com>
2025-06-23 16:31:23 +0530

    update stun2

--- frontend/src/hooks/useWebRTC.ts
+++ frontend/src/hooks/useWebRTC.ts
@@ -141,19 +141,10 @@ export function useWebRTC(
             const p = new RTCPeerConnection({
                 iceServers: [
                     { urls: "stun:stun.l.google.com:19302" },
-                    // { urls: "stun:stun1.l.google.com:19302" },
-                    // { urls: "stun:stun2.l.google.com:19302" },
-                    // { urls: "stun:stun3.l.google.com:19302" },
-                    // { urls: "stun:stun4.l.google.com:19302" },
                     {
-                        urls: "turn:openrelay.metered.ca:80",
-                        username: "openrelayproject",
-                        credential: "openrelayproject"
-                    },
-                    {
-                        urls: "turn:openrelay.metered.ca:443",
-                        username: "openrelayproject",
-                        credential: "openrelayproject"
+                        urls: "turn:relay1.expressturn.com:3478",
+                        username: "efkFzYkQzZy1n3Q",
+                        credential: "2Qk4v7w6w8y9"
                     }
                 ]
             });
