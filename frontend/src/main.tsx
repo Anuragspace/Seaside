86d315c262cd571e261939f82335e3d4ab823ea9
Anurag Adarsh <105537288+Anuragspace@users.noreply.github.com>
2025-07-12 23:31:53 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,11 @@ import App from './App';
 import './index.css';
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+/* const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
-}
+} */
 
 createRoot(document.getElementById('root')!).render(
   <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
